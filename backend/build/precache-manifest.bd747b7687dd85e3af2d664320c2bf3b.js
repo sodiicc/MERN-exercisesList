0ef2a24efc638366d69d71afff9dd372ba6beca0
Tsyvinskyi Vasiliy <sodiicc@gmail.com>
2019-10-03 16:13:42 +0300

    add build back

---
+++ backend/build/precache-manifest.bd747b7687dd85e3af2d664320c2bf3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8305bbb7ca0586d25d1d810fef900542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e77110da1b5405c6893d",
+    "url": "/static/css/2.a7b3a3cd.chunk.css"
+  },
+  {
+    "revision": "419fd29e5046997de860",
+    "url": "/static/css/main.d099d210.chunk.css"
+  },
+  {
+    "revision": "e77110da1b5405c6893d",
+    "url": "/static/js/2.03a5469d.chunk.js"
+  },
+  {
+    "revision": "419fd29e5046997de860",
+    "url": "/static/js/main.f33eb164.chunk.js"
+  },
+  {
+    "revision": "2e779842f6a47fa25469",
+    "url": "/static/js/runtime-main.e16beb6f.js"
+  }
+]);
